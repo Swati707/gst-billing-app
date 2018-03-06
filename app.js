e0608296c7ab51e498339118286372ece2735af4
Swati707 <swatikanchan707@gmail.com>
2018-03-06 12:37:39 +0530

    connected to the mysql server

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const Hapi = require('hapi');
+const mysql = require('mysql');
 
+// server connection();
 const server = new Hapi.Server({
     host: "localhost",
     port: 3000,
@@ -8,7 +10,38 @@ const server = new Hapi.Server({
     }
 });
 
-// server.connection();
+//Start the server
+async function startServer() {
+    try {
+        await server.start();
+    }
+    catch (err) {
+        console.log(err);
+        process.exit(1);
+    }
+    console.log('Server running at:', server.info.uri);
+};
+startServer();
+
+//connect to mysql server
+var db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: '',
+    database: "ps_gst_billing_task"
+});
+
+db.connect((err) => {
+    if(err){
+        console.log("Error while connecting to mysql");
+        process.exit(1);
+    }
+    console.log("Connected to mysql.");
+});
+db.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+    if (error) throw error;
+    console.log('The solution is: ', results[0].solution);
+});
 
 server.route({
     method: "GET",
@@ -17,26 +50,3 @@ server.route({
         return("hellow world!");
     }
 });
-
-// server.start(error => {
-//     if(error){
-//         console.log("Error while starting server");
-//         throw error;
-//     }
-//     console.log("Server running at: " + server.info.uri);
-// });
-// Start the server
-async function start() {
-
-    try {
-        await server.start();
-    }
-    catch (err) {
-        console.log(err);
-        process.exit(1);
-    }
-
-    console.log('Server running at:', server.info.uri);
-};
-
-start();
